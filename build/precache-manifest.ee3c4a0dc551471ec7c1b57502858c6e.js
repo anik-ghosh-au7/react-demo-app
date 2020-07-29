bc22ae58a4ee1716c6bb74529b3c7991f51ebb56
Anik Ghosh <tech.anikghosh@gmail.com>
2020-07-29 22:37:58 +0530

    react heroku initial commit

---
+++ build/precache-manifest.ee3c4a0dc551471ec7c1b57502858c6e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f66bad83f006227ba4973b61f8da91f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b291b1db8f7397d887d",
+    "url": "/static/js/2.95e004cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.95e004cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92abb397ef46b3ab9f08",
+    "url": "/static/js/main.d86dfae7.chunk.js"
+  },
+  {
+    "revision": "a43cc78d821fc5d25988",
+    "url": "/static/js/runtime-main.a6770dc5.js"
+  }
+]);
